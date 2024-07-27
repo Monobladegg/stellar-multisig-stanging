9bf83f7872de975df29b4adddbed56a3ebf84e2e
Monoblade <157546326+Monobladegg@users.noreply.github.com>
2024-07-27 12:06:26 +0300

    fix

--- app/public/[id]/page.js
+++ app/public/[id]/page.js
@@ -1,20 +1,18 @@
 // pages/public/[id].js
-
-import axios from "axios";
+import axios from 'axios';
 import PublicNet from "./publicnet"; // Adjust the import path as needed
-import StellarSdk from "stellar-sdk";
-import MainLayout from "@/components/layouts";
+import StellarSdk from 'stellar-sdk';
+import MainLayout from '@/components/layouts';
 
 // Fetching account IDs using Stellar Expert API
 const getAccountIds = async () => {
-    const apiStellarURI =
-        "https://api.stellar.expert/explorer/directory?limit=20";
+    const apiStellarURI = "https://api.stellar.expert/explorer/directory?limit=20";
     try {
         const response = await axios.get(apiStellarURI);
         const accounts = response.data._embedded.records;
-        return accounts.map((account) => ({ id: account.address }));
+        return accounts.map(account => ({ id: account.address }));
     } catch (error) {
-        console.error("Error fetching accounts from Stellar Expert:", error);
+        console.error('Error fetching accounts from Stellar Expert:', error);
         return [];
     }
 };
@@ -22,30 +20,24 @@ const getAccountIds = async () => {
 // Function to generate static params
 export async function generateStaticParams() {
     const accounts = await getAccountIds();
-    const params = accounts.map((account) => ({ id: account.id }));
+    const params = accounts.map(account => ({ id: account.id }));
     return params;
 }
 
-// Client-side component
-const ClientComponent = ({ params }) => {
+// Page component
+export default function Page({ params }) {
     if (!StellarSdk.StrKey.isValidEd25519PublicKey(params.id)) {
-        return (
-            <div className="container">
-                <div className="search error container narrow" style={{ padding: "20px" }}>
+        // Handle case where params are not found or invalid
+        return (<><MainLayout>
+            <div className="cotainer">
+                <div className={`search error container narrow`} style={{ padding: '20px' }} >
                     <h2 className="text-overflow">Search results for {params.id}</h2>
                     <div>User ID not found or invalid.</div>
                 </div>
             </div>
-        );
+        </MainLayout>
+        </>)
+
     }
     return <PublicNet params={params} />;
-};
-
-// Page component
-export default async function Page({ params }) {
-    return (
-        <MainLayout>
-            <ClientComponent params={params} />
-        </MainLayout>
-    );
 }

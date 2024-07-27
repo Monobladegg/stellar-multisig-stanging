0ddc199fbea5b3d2c3e06254cee34bb2d1e252fc
Monoblade <157546326+Monobladegg@users.noreply.github.com>
2024-07-27 11:47:34 +0300

    fix

--- app/public/[id]/page.js
+++ app/public/[id]/page.js
@@ -1,10 +1,48 @@
 // pages/public/[id].js
+
 import axios from "axios";
-import PublicNet from "./publicnet"; // Adjust the import path as needed
+import PublicNet from "./publicnet";
 import StellarSdk from "stellar-sdk";
 import MainLayout from "@/components/layouts";
 
-// Fetching account IDs using Stellar Expert API
+export async function getStaticPaths() {
+    const accounts = await getAccountIds();
+    const paths = accounts.map((account) => ({
+        params: { id: account.id },
+    }));
+
+    return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+    return {
+        props: {
+            id: params.id,
+        },
+    };
+}
+
+export default function Page({ id }) {
+    if (!StellarSdk.StrKey.isValidEd25519PublicKey(id)) {
+        return (
+            <MainLayout>
+                <div className="container">
+                    <div
+                        className={`search error container narrow`}
+                        style={{ padding: "20px" }}
+                    >
+                        <h2 className="text-overflow">
+                            Search results for {id}
+                        </h2>
+                        <div>User ID not found or invalid.</div>
+                    </div>
+                </div>
+            </MainLayout>
+        );
+    }
+    return <PublicNet id={id} />;
+}
+
 const getAccountIds = async () => {
     const apiStellarURI =
         "https://api.stellar.expert/explorer/directory?limit=20";
@@ -17,38 +55,3 @@ const getAccountIds = async () => {
         return [];
     }
 };
-
-// Function to generate static params
-export async function generateStaticParams() {
-    const accounts = await getAccountIds();
-    const params = accounts.map((account) => ({ id: account.id }));
-    return params;
-}
-
-
-// Page component
-export default function Page({ params }) {
-    generateStaticParams()
-    if (!StellarSdk.StrKey.isValidEd25519PublicKey(params.id)) {
-        // Handle case where params are not found or invalid
-        return (
-            <>
-                <MainLayout>
-                    <div className="cotainer">
-                        <div
-                            className={`search error container narrow`}
-                            style={{ padding: "20px" }}
-                        >
-                            <h2 className="text-overflow">
-                                Search results for {params.id}
-                            </h2>
-                            <div>User ID not found or invalid.</div>
-                        </div>
-                    </div>
-                </MainLayout>
-            </>
-        );
-    }
-    return <PublicNet params={params} />;
-}
-// 

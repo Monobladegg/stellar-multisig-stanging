ad2720443d9a1f79b40c999ad0ddd5d7f9b342c9
Monoblade <157546326+Monobladegg@users.noreply.github.com>
2024-07-27 14:56:13 +0300

    test

--- app/public/[id]/page.js
+++ app/public/[id]/page.js
@@ -1,12 +1,5 @@
 // app/public/[id]/page.js
 
-// Секция для серверной логики
-export async function generateStaticParams() {
-    // Пример данных, которые вы можете получить из API или базы данных
-    const staticAccountIds = ["GAEAAQ6GYMF6SSAWSW3MSLWB5LHYQIEGF3I3TUBJM6L3MSZYSPOMTLTH"];
-    return staticAccountIds.map(id => ({ id }));
-}
-
 // Компонент страницы
 import MainLayout from "@/components/layouts";
 import ClientComponent from "./ClientComponent";

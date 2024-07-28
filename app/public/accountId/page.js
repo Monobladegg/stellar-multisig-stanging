5329b90a8814788f78b31b3a659e1757f416d188
Monoblade <157546326+Monobladegg@users.noreply.github.com>
2024-07-28 13:07:01 +0300

    ewsd

--- app/public/accountId/page.js
+++ app/public/accountId/page.js
@@ -1,42 +1,28 @@
-// app/public/accountId?id=
-
+// pages/public/accountId/[id].js
 "use client";
 
 import MainLayout from "@/components/layouts";
 import PublicNet from "./publicnet";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
-// Функция для получения данных
-// const getAccountIds = async () => {
-//     const apiStellarURI = "https://api.stellar.expert/explorer/directory?limit=20";
-//     try {
-//         const response = await axios.get(apiStellarURI);
-//         const accounts = response.data._embedded.records;
-//         console.log(accounts);
-//         return accounts;
-//     } catch (error) {
-//         console.error("Error fetching accounts from Stellar Expert:", error);
-//         return [];
-//     }
-// };
-
-// Компонент страницы
 const Page = () => {
 
-    const url = window.location.href
-    // http://localhost:3000/public/accountId?id=GAEAAQ6GYMF6SSAWSW3MSLWB5LHYQIEGF3I3TUBJM6L3MSZYSPOMTLTH
-    const query = new URLSearchParams(url.split('?')[1]);
-    const id = query.get('id');
+    const [href, setHref] = useState("");
+    useEffect(() => {
+        setHref(window.location.href);
+    }, []);
+    console.log(href);
+
+    const id = href.split("id=").pop();
+    console.log(id)
 
     return (
-        <MainLayout>
-            <PublicNet id={id} />
-        </MainLayout>
+        // <MainLayout>
+        //     {id ? <PublicNet id={id} /> : <p>Loading...</p>}
+        // </MainLayout>
+        123
     );
 };
-
-// export async function generateStaticParams() {
-//     const accounts = await getAccountIds();
-//     return accounts.map(account => ({ id: account.address }));
-// }
 
 export default Page;

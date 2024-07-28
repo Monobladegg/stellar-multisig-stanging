4a338f1ba8814a136a032c6cb2bfb10836a52a70
Monoblade <157546326+Monobladegg@users.noreply.github.com>
2024-07-28 12:12:51 +0300

    q

--- app/public/[id]/page.js
+++ app/public/[id]/page.js
@@ -1,6 +1,31 @@
-import MainLayout from "@/components/layouts";
-import PublicNet from "./publicnet";
+// app/public/[id]/page.js
 
+import axios from 'axios';
+import MainLayout from "@/components/layouts";
+import ClientComponent from "./ClientComponent";
+
+// Секция для серверной логики
+const getAccountIds = async () => {
+    const apiStellarURI = "https://api.stellar.expert/explorer/directory?limit=20";
+    try {
+        const response = await axios.get(apiStellarURI);
+        const accounts = response.data._embedded.records;
+        console.log(accounts);
+        return accounts;
+    } catch (error) {
+        console.error("Error fetching accounts from Stellar Expert:", error);
+        return [];
+    }
+};
+
+export async function generateStaticParams() {
+    // Пример данных, которые вы можете получить из API или базы данных
+    const accounts = await getAccountIds();
+    return accounts.map(account => ({ id: account.address }));
+}
+//
+
+// Компонент страницы
 const Page = ({ params }) => {
 
     const {id} = params

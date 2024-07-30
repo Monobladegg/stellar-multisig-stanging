fcb87fb461fafa5d3297d5764eeca301015d1716
Monoblade <157546326+Monobladegg@users.noreply.github.com>
2024-07-30 12:30:00 +0300

    q

--- app/public/[id]/page.js
+++ app/public/[id]/page.js
@@ -1,51 +1,36 @@
-// pages/public/[id].js
-import axios from "axios";
-import PublicNet from "./publicnet"; // Adjust the import path as needed
-import StellarSdk from "stellar-sdk";
-import MainLayout from "@/components/layouts";
+// app/public/[id].js
 
-// Fetching account IDs using Stellar Expert API
-const getAccountIds = async () => {
-    const apiStellarURI =
-        "https://api.stellar.expert/explorer/directory?limit=20";
-    try {
-        const response = await axios.get(apiStellarURI);
-        const accounts = response.data._embedded.records;
-        return accounts.map((account) => ({ id: account.address }));
-    } catch (error) {
-        console.error("Error fetching accounts from Stellar Expert:", error);
-        return [];
-    }
-};
+// import useSWR from "swr";
+import PublicNet from "./publicnet";
 
-// Function to generate static params
-export async function generateStaticParams() {
-    const accounts = await getAccountIds();
-    const params = accounts.map((account) => ({ id: account.id }));
-    return params;
-}
+// const getAccountIds = async () => {
+//     const apiStellarURI =
+//         "https://api.stellar.expert/explorer/directory?limit=20";
+//     try {
+//         const response = await axios.get(apiStellarURI);
+//         const accounts = response.data._embedded.records;
+//         return accounts.map((account) => ({ id: account.address }));
+//     } catch (error) {
+//         console.error("Error fetching accounts from Stellar Expert:", error);
+//         return [];
+//     }
+// };
 
-// Page component
-export default function Page({ params }) {
-    if (!StellarSdk.StrKey.isValidEd25519PublicKey(params.id)) {
-        // Handle case where params are not found or invalid
-        return (
-            <>
-                <MainLayout>
-                    <div className="cotainer">
-                        <div
-                            className={`search error container narrow`}
-                            style={{ padding: "20px" }}
-                        >
-                            <h2 className="text-overflow">
-                                Search results for {params.id}
-                            </h2>
-                            <div>User ID not found or invalid.</div>
-                        </div>
-                    </div>
-                </MainLayout>
-            </>
-        );
-    }
+// export async function generateStaticParams({params: {id}}) {
+//     const accounts = await getAccountIds();
+//     const params = accounts.map((account) => ({ id: account.id }));
+//     return params;
+// }
+
+export default function Page({params}) {
     return <PublicNet id={params.id} />;
 }
+
+// const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+// function Content({ id }) {
+//     const { data, error } = useSWR([`https://horizon.stellar.org/accounts/${id}`], fetcher);
+//     if (error) return <div>Failed to load</div>;
+//     if (!data) return <div>Loading...</div>;
+//     return <PublicNet id={id} />;
+// }

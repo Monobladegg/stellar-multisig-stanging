02d9320fe5468d57be31b81423b5482776cae04f
Monoblade <157546326+Monobladegg@users.noreply.github.com>
2024-07-30 12:54:51 +0300

    h

--- app/public/[id]/page.js
+++ app/public/[id]/page.js
@@ -1,8 +1,9 @@
-// app/public/[id].js
-
 import axios from "axios";
-import PublicNet from "./publicnet";
+import PublicNet from "./publicnet"; // Adjust the import path as needed
+import StellarSdk from "stellar-sdk";
+import MainLayout from "@/components/layouts";
 
+// Fetching account IDs using Stellar Expert API
 const getAccountIds = async () => {
     const apiStellarURI =
         "https://api.stellar.expert/explorer/directory?limit=20";
@@ -16,21 +17,34 @@ const getAccountIds = async () => {
     }
 };
 
+// Function to generate static params
 export async function generateStaticParams() {
     const accounts = await getAccountIds();
     const params = accounts.map((account) => ({ id: account.id }));
     return params;
 }
 
-export default function Page({params}) {
-    return <PublicNet id={params.id} />;
+// Page component
+export default function Page({ params }) {
+    if (!StellarSdk.StrKey.isValidEd25519PublicKey(params.id)) {
+        // Handle case where params are not found or invalid
+        return (
+            <>
+                <MainLayout>
+                    <div className="cotainer">
+                        <div
+                            className={`search error container narrow`}
+                            style={{ padding: "20px" }}
+                        >
+                            <h2 className="text-overflow">
+                                Search results for {params.id}
+                            </h2>
+                            <div>User ID not found or invalid.</div>
+                        </div>
+                    </div>
+                </MainLayout>
+            </>
+        );
+    }
+    return <PublicNet params={params} />;
 }
-
-// const fetcher = (...args) => fetch(...args).then((res) => res.json());
-
-// function Content({ id }) {
-//     const { data, error } = useSWR([`https://horizon.stellar.org/accounts/${id}`], fetcher);
-//     if (error) return <div>Failed to load</div>;
-//     if (!data) return <div>Loading...</div>;
-//     return <PublicNet id={id} />;
-// }

4a2f9e90bfe9b2256598fdb54fdd9cc7e1cd3fd7
Monoblade <157546326+Monobladegg@users.noreply.github.com>
2024-07-31 10:29:34 +0300

    с

--- app/public/[id]/page.js
+++ app/public/[id]/page.js
@@ -1,3 +1,4 @@
+// pages/public/[id].js
 import axios from "axios";
 import PublicNet from "./publicnet"; // Adjust the import path as needed
 import StellarSdk from "stellar-sdk";
@@ -19,31 +20,29 @@ const getAccountIds = async () => {
 
 // Function to generate static params
 export async function generateStaticParams() {
-    const accounts = await getAccountIds();
+    let accounts = await getAccountIds();
+    
+    // Manually add missing IDs if needed
+    accounts.push({ id: 'GAEAAQ6GYMF6SSAWSW3MSLWB5LHYQIEGF3I3TUBJM6L3MSZYSPOMTLTH' });
+
     const params = accounts.map((account) => ({ id: account.id }));
+    console.log('Generated Static Params:', params); // Debugging line
     return params;
 }
+
 
 // Page component
 export default function Page({ params }) {
     if (!StellarSdk.StrKey.isValidEd25519PublicKey(params.id)) {
-        // Handle case where params are not found or invalid
         return (
-            <>
-                <MainLayout>
-                    <div className="cotainer">
-                        <div
-                            className={`search error container narrow`}
-                            style={{ padding: "20px" }}
-                        >
-                            <h2 className="text-overflow">
-                                Search results for {params.id}
-                            </h2>
-                            <div>User ID not found or invalid.</div>
-                        </div>
+            <MainLayout>
+                <div className="container">
+                    <div className="search error container narrow" style={{ padding: "20px" }}>
+                        <h2 className="text-overflow">Search results for {params.id}</h2>
+                        <div>User ID not found or invalid.</div>
                     </div>
-                </MainLayout>
-            </>
+                </div>
+            </MainLayout>
         );
     }
     return <PublicNet params={params} />;

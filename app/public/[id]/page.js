efbb17cb49608da1d7bec189ee4613bcccbb5507
Monoblade <157546326+Monobladegg@users.noreply.github.com>
2024-07-31 20:25:26 +0300

    f

--- app/public/[id]/page.js
+++ app/public/[id]/page.js
@@ -4,25 +4,25 @@ import PublicNet from "./publicnet"; // Adjust the import path as needed
 import StellarSdk from "stellar-sdk";
 import MainLayout from "@/components/layouts";
 
-// // Fetching account IDs using Stellar Expert API
-// const getAccountIds = async () => {
-//     const apiStellarURI = "https://api.stellar.expert/explorer/directory?limit=20";
-//     try {
-//         const response = await axios.get(apiStellarURI);
-//         const accounts = response.data._embedded.records;
-//         return accounts.map(account => ({ id: account.address }));
-//     } catch (error) {
-//         console.error('Error fetching accounts from Stellar Expert:', error);
-//         return [];
-//     }
-// };
+// Fetching account IDs using Stellar Expert API
+const getAccountIds = async () => {
+    const apiStellarURI = "https://api.stellar.expert/explorer/directory?limit=20";
+    try {
+        const response = await axios.get(apiStellarURI);
+        const accounts = response.data._embedded.records;
+        return accounts.map(account => ({ id: account.address }));
+    } catch (error) {
+        console.error('Error fetching accounts from Stellar Expert:', error);
+        return [];
+    }
+};
 
-// // Function to generate static params
-// export async function generateStaticParams() {
-//     const accounts = await getAccountIds();
-//     const params = accounts.map(account => ({ id: account.id }));
-//     return params;
-// }
+// Function to generate static params
+export async function generateStaticParams() {
+    const accounts = await getAccountIds();
+    const params = accounts.map(account => ({ id: account.id }));
+    return params;
+}
 
 // Page component
 export default function Page({ params }) {

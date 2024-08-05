c415e435b0b53db7263105dd2bedce9b01002ca1
Monoblade <157546326+Monobladegg@users.noreply.github.com>
2024-08-05 13:06:25 +0300

    г

---
+++ app/public/page.tsx
@@ -0,0 +1,9 @@
+import SearchBar from "@/components/Search";
+
+export default function Home() {
+    return (
+        <div className="page-container">
+            <SearchBar />
+        </div>
+    );
+}

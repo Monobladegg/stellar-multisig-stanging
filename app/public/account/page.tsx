354a7078f9bf6d00cafcba7d4493e4fdc442e9fd
Monoblade <157546326+Monobladegg@users.noreply.github.com>
2024-08-07 11:37:34 +0300

    q

--- app/public/account/page.tsx
+++ app/public/account/page.tsx
@@ -1,8 +1,17 @@
 import React, { FC, Suspense } from "react";
 import Account from "@/pages/public/account/account";
+import { MainLayout } from "@/widgets";
 
 const Page: FC = () => (
-    <Suspense>
+    <Suspense
+        fallback={
+            <MainLayout>
+                <center>
+                    <h1>Loading...</h1>
+                </center>
+            </MainLayout>
+        }
+    >
         <Account />
     </Suspense>
 );

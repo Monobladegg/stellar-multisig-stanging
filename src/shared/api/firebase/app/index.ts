d25706c2b49ed03117dd05857607840118131c15
Monoblade <157546326+Monobladegg@users.noreply.github.com>
2024-09-18 06:16:30 +0300

    trg

--- src/shared/api/firebase/app/index.ts
+++ src/shared/api/firebase/app/index.ts
@@ -1,7 +1,7 @@
 
 import { initializeApp } from "firebase/app";
 
-const firebaseApp = initializeApp({
+const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY || process.env.NEXT_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN || process.env.NEXT_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID || process.env.NEXT_PROJECT_ID,
@@ -9,6 +9,15 @@ const firebaseApp = initializeApp({
   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID || process.env.NEXT_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_APP_ID || process.env.NEXT_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID || process.env.NEXT_MEASUREMENT_ID
-});
+}
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const requiredFields = ['apiKey', 'authDomain', 'projectId', 'storageBucket', 'messagingSenderId', 'appId', 'measurementId'];
+for (const field of requiredFields) {
+  if (!(field in firebaseConfig) || !firebaseConfig[field as keyof typeof firebaseConfig]) {
+    throw new Error(`Firebase configuration error: ${field} is missing.`);
+  }
+}
 
 export default firebaseApp;

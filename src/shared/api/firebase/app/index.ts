e6ffada1cf63a846f86a4352b80e84015f7c36d2
Monoblade <157546326+Monobladegg@users.noreply.github.com>
2024-09-18 17:38:21 +0300

    qwes

--- src/shared/api/firebase/app/index.ts
+++ src/shared/api/firebase/app/index.ts
@@ -2,13 +2,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+  apiKey: localStorage.getItem("Firebase-apiKey") || process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: localStorage.getItem("Firebase-authDomain") || process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: localStorage.getItem("Firebase-projectId") || process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: localStorage.getItem("Firebase-storageBucket") || process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: localStorage.getItem("Firebase-messagingSenderId") || process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: localStorage.getItem("Firebase-appId") || process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: localStorage.getItem("Firebase-measurementId") || process.env.NEXT_PUBLIC_MEASUREMENT_ID
 }
 
 const firebaseApp = initializeApp(firebaseConfig);

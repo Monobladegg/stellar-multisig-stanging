48b84059eabe01558402441221dac9a65569417b
Monoblade <157546326+Monobladegg@users.noreply.github.com>
2024-10-17 22:24:12 +0300

    qwda

--- getCommitHash.mjs
+++ getCommitHash.mjs
@@ -3,7 +3,7 @@
 import { execSync } from "child_process";
 
 const getCommitHash = () => {
-    return execSync("git rev-parse --short HEAD").toString().trim();
-}
+  return execSync("git rev-parse --short HEAD").toString().trim();
+};
 
 export default getCommitHash;
